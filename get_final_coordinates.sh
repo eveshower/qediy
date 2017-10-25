af3bc668ac39a0bbea65fa75ef510af16f281037
eveshower <eveshower@gmail.com>
2017-10-25 09:38:07 +0800

    Update get_final_coordinates.sh

--- get_final_coordinates.sh
+++ get_final_coordinates.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-awk  'BEGIN{
-    n=0;
-    tot=0;
-}
-/Begin final coordinates/,/End final coordinates/{
+awk  '/Begin final coordinates/,/End final coordinates/{
         print $0
 }' $1 |awk 'NR>=4 && !/End final coordinates/{print $0}' > finalcoord.dat
